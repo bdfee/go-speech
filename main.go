0e8b818776012b4a1aefaf2ed881f650c52326eb
Ben Durfee <ben.p.durfee@gmail.com>
2023-08-26 08:16:46 -0400

    init

--- main.go
+++ main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	// "context"
+	// "fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	// "github.com/joho/godotenv"
+	// openai "github.com/sashabaranov/go-openai"
 )
+
+// use godot package to load/read the .env file and
+// return the value of the key
+// func goDotEnvVariable(key string) string {
+
+// 	// load .env file
+// 	err := godotenv.Load(".env")
+
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file")
+// 	}
+
+// 	return os.Getenv(key)
+// }
+
+// func openAi() {
+// 	client := openai.NewClient(goDotEnvVariable("GPT_API_KEY"))
+// 	ctx := context.Background()
+
+// 	req := openai.AudioRequest{
+// 		Model:    openai.Whisper1,
+// 		FilePath: "recording.mp3",
+// 	}
+
+// 	resp, err := client.CreateTranscription(ctx, req)
+
+// 		if err != nil {
+// 		log.Printf("Transcription error: %v\n", err)
+// 		return
+// 	}
+// 	log.Println(resp.Text)
+
+// }
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", serveTemplate)
-
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
